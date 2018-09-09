ed7473e7d16956bc0eb7bc265ccd783c0df5e6b4
Yago Vega <yagofvv@gmail.com>
2018-09-09 17:51:12 +0200

    fetching api data and display authors working

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
-  state = {
-    posts: []
+  constructor(props) {
+    super(props);
+    this.state = { isLoading: true };
   }
+  componentDidMount() {
+    return fetch("https://api.reddit.com/r/pics/new.json")
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          dataList: responseJson
+        }).catch(error => {
+          console.error(error);
+        });
+      })
+      .catch(error => console.error(error));
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
-    );
+    if (!this.state.isLoading) {
+      return this.state.dataList.data.children.map(item => (
+        <Text>{item.data.author_fullname}</Text>
+      ));
+    } else {
+      return <Text>Loading...</Text>;
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
